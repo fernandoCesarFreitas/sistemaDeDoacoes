779a9dc9e27dee8c80cfc89e0896c500957cc633
ViniSouza32 <134454570+ViniSouza32@users.noreply.github.com>
2023-09-12 18:11:25 -0300

    Update app.ts
    menu dos beneficiarios

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import PromptSync from "prompt-sync";
 import DB from "./db";// esse import conecta com o banco
+import { BeneficiariosMenu } from "./views/BeneficiariosMenu";
 
 const prompt = PromptSync();
+
+let input: string | null = " ";
 
 async function main(): Promise<void> {
   await DB.initialize();
 
+  let beneficiariosMenu = new BeneficiariosMenu();
+
+
+  do {
+    console.clear();
+    beneficiariosMenu.show();
+    console.log("0 - Sair");
+    input = prompt("Selecione a opção desejada: ");
+
+    if (input != "0") {
+      await beneficiariosMenu.execute(input);
+      prompt("Pressione enter para continuar");
+    }
+  } while (input != "0");
   
 }
 
