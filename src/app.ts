69d1b697b54eea8ad33fdfe449f08196dda47f57
ViniSouza32 <134454570+ViniSouza32@users.noreply.github.com>
2023-09-13 19:27:54 -0300

    Update app.ts
    imports e um modelo de menu

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import PromptSync from "prompt-sync";
 import DB from "./db"; // esse import conecta com o bamco
 import { Menu } from "./views/Menu";
+import { BeneficiariosMenu } from "./views/BeneficiariosMenu";
+import { CategoriasMenu } from "./views/CategoriasMenu";
 
 const prompt = PromptSync();
 
@@ -9,6 +11,24 @@ async function main(): Promise<void> {
   await DB.initialize();
   const menu = new Menu();
   menu.menu();
+
+  // let beneficiariosMenu = new BeneficiariosMenu();
+  // let cadegoriasMenu = new CategoriasMenu();
+
+
+  // do {
+  //   console.clear();
+  //   beneficiariosMenu.show();
+  //   cadegoriasMenu.show();
+  //   console.log("0 - Sair");
+  //   input = prompt("Selecione a opção desejada: ");
+
+  //   if (input != "0") {
+  //     await beneficiariosMenu.execute(input);
+  //     await cadegoriasMenu.execute(input);
+  //     prompt("Pressione enter para continuar");
+  //   }
+  // } while (input != "0");
 }
 
 main();
