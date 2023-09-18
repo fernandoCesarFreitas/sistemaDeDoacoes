0f6050d8e5489e48f9cec6613db1d2c3e342939d
ViniSouza32 <134454570+ViniSouza32@users.noreply.github.com>
2023-09-18 04:12:04 -0300

    controller da movimentacao
    controller da movimentacao

---
+++ src/controllers/MovimentacaoController.ts
@@ -0,0 +1,40 @@
+import { Movimentacao } from "../models/Movimentacao";
+import { Pessoas } from "../models/Pessoas";
+import { CdItem } from "../models/cd_item";
+
+export class MovimentacaoContrller {
+
+  async list() {
+    return await Movimentacao.find();
+  }
+
+  async create (data_Hora: string, tipo: string, quantidade: number, doador: string) {
+    
+    return await Movimentacao.create({
+      data_Hora,
+      tipo,
+      quantidade,
+      doador,
+    }).save();
+  }
+
+  
+  async find (id_movimentacao: number) {
+    return await Movimentacao.findOneBy({ id_movimentacao });
+  }
+
+  async edit(movimentacao: Movimentacao, data_Hora: string, tipo: string, quantidade: number, doador: string) {
+    movimentacao.data_Hora = data_Hora;
+    movimentacao.tipo = tipo;
+    movimentacao.quantidade = quantidade;
+    movimentacao.doador = doador;
+    await movimentacao.save();
+
+    return movimentacao;
+  }
+  
+  async delete(movimentacao: Movimentacao): Promise<void> {
+    await movimentacao.remove();
+  }
+
+}
