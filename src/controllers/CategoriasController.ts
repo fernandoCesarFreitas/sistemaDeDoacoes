2b13910cf53e169cddd75503d6e8ed311aa9583a
Fernado Cesar Haupenthal de Freitas <139494640+fernandoCesarFreitas@users.noreply.github.com>
2023-09-26 22:26:42 -0300

    funcionando categorias

--- src/controllers/CategoriasController.ts
+++ src/controllers/CategoriasController.ts
@@ -1,10 +1,14 @@
 import { Categoria } from "../models/Categorias";
+import { Request, Response } from "express";
 
 export class CategoriasController {
 
-  async list() {
-    return await Categoria.find({relations: ['itens']});
-  }
+  async list (req: Request, res: Response):Promise<Response>{
+    let categorias: Categoria[] = await Categoria.find()
+    console.log(categorias);
+     return res.status(200).json({categorias})
+ }
+ 
 
   async create (descricao: string) {
     
