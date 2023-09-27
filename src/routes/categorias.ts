63d0e9e504fdd905085939756ea301951ad35902
Fernado Cesar Haupenthal de Freitas <139494640+fernandoCesarFreitas@users.noreply.github.com>
2023-09-26 21:31:40 -0300

    routes

---
+++ src/routes/categorias.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { CategoriasController } from "../controllers/CategoriasController";
+let categoriaController: CategoriasController = new CategoriasController();
+
+let rotas:Router = Router();
+//listar
+rotas.get("/categorias", categoriaController.list);
+//visualizar 1 usuario pelo id
+rotas.get("/categorias/:id",categoriaController.find);
+//criar
+rotas.post("/categorias",categoriaController.create);
+//atualizar
+rotas.put("/categorias/:id",categoriaController.edit);
+//delete
+rotas.delete("/categorias/:id",categoriaController.delete);
+
+export default rotas;
