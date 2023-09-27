cbed409f8fe073d32bbabbd3c66b52fa42139314
Fernado Cesar Haupenthal de Freitas <139494640+fernandoCesarFreitas@users.noreply.github.com>
2023-09-27 10:40:37 -0300

    API categorias 100% funcional

--- src/controllers/CategoriasController.ts
+++ src/controllers/CategoriasController.ts
@@ -2,35 +2,60 @@ import { Categoria } from "../models/Categorias";
 import { Request, Response } from "express";
 
 export class CategoriasController {
-
-  async list (req: Request, res: Response):Promise<Response>{
-    let categorias: Categoria[] = await Categoria.find()
+  async list(req: Request, res: Response): Promise<Response> {
+    let categorias: Categoria[] = await Categoria.find();
     console.log(categorias);
-     return res.status(200).json({categorias})
- }
- 
+    return res.status(200).json({ categorias });
+  }
 
-  async create (descricao: string) {
-    
-    return await Categoria.create({
-      descricao,
+  async create(req: Request, res: Response): Promise<Response> {
+    let body = req.body;
+
+    let categoria: Categoria = await Categoria.create({
+      descricao: body.descricao,
     }).save();
+    return res.status(200).json(categoria);
   }
 
-  
-  async find (id_categoria: number) {
-    return await Categoria.findOneBy({ id_categoria });
-  }
+  async edit(req: Request, res: Response): Promise<Response> {
+    let body = req.body;
 
-  async edit(categoria: Categoria, descricao: string) {
-    categoria.descricao = descricao;
+    let id: number = Number(req.params.id);
+    console.log(id)
+    let categoria: Categoria | null = await Categoria.findOneBy({ id_categoria:id });
+
+    if (!categoria) {
+      return res.status(422).json({ error: "Categoria não encontrada!" });
+    }
+    categoria.descricao = body.descricao;
     await categoria.save();
 
-    return categoria;
-  }
-  
-  async delete(categoria: Categoria): Promise<void> {
-    await categoria.remove();
+    return res.status(200).json(categoria);
   }
 
+  async delete (req: Request, res: Response): Promise<Response>{
+    let body = req.body;
+    let id = Number(req.params.id);
+
+    let categoria: Categoria | null =  await Categoria.findOneBy({id_categoria:id});
+
+    if(!categoria){
+        return res.status(422).json({error:'produto não encontrado'});
+    }
+    categoria.remove();
+    return res.status(200).json();
+}
+
+  async find(req: Request, res: Response): Promise<Response> {
+    let body = req.body;
+    let id = Number(req.params.id);
+
+    let categoria: Categoria | null = await Categoria.findOneBy({ id_categoria:id });
+
+    if (!categoria) {
+      return res.status(422).json({ error: "produto não encontrado" });
+    }
+
+    return res.status(200).json(categoria);
+  }
 }
