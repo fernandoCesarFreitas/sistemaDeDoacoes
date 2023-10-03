cadb4b25c3a7fa92fde128e385eef90e0cd08a0a
Fernado Cesar Haupenthal de Freitas <139494640+fernandoCesarFreitas@users.noreply.github.com>
2023-10-03 19:06:56 -0300

    tela e API voluntarios

--- src/controllers/VoluntariosController.ts
+++ src/controllers/VoluntariosController.ts
@@ -1,5 +1,6 @@
 import { Voluntario } from "../models/Voluntarios";
 import * as bcrypt from "bcrypt";
+import { ILike } from "typeorm";
 import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 let user: Voluntario = new Voluntario();
@@ -7,27 +8,37 @@ let userId: Voluntario;
 import { Request, Response } from "express";
 
 export class VoluntarioController {
-
   async list(req: Request, res: Response): Promise<Response> {
-    let voluntarios: Voluntario[] = await Voluntario.find();
+    let nome = req.query.nome;
 
-    return res.status(200).json(voluntarios);
+    let voluntario: Voluntario[] = await Voluntario.findBy({
+      nome: nome ? ILike(`%${nome}%`) : undefined,
+    });
+    return res.status(200).json(voluntario);
   }
+  // async list(req: Request, res: Response): Promise<Response> {
+  //   let voluntarios: Voluntario[] = await Voluntario.find();
+
+  //   return res.status(200).json(voluntarios);
+  // }
 
   async listActive(req: Request, res: Response): Promise<Response> {
-    let voluntarios: Voluntario[] = await Voluntario.find({ where: { situacao: "A" }});
+    let voluntarios: Voluntario[] = await Voluntario.find({
+      where: { situacao: "A" },
+    });
 
     return res.status(200).json(voluntarios);
   }
 
   async create(req: Request, res: Response): Promise<Response> {
     let body = req.body;
-
+    const senhaEncryptada = await bcrypt.hash(body.senha, 10);
+    console.log(senhaEncryptada);
     let voluntario: Voluntario = await Voluntario.create({
       nome: body.nome,
-      senha: body.senha,
+      senha: senhaEncryptada,
       email: body.email,
-      situacao: 'A',
+      situacao: "A",
     }).save();
 
     return res.status(200).json(voluntario);
@@ -35,37 +46,50 @@ export class VoluntarioController {
 
   async edit(req: Request, res: Response): Promise<Response> {
     let body = req.body;
-    let id = Number(req.params.id);
+    let voluntario: Voluntario = res.locals.voluntario;
+    // let categoria: Categoria | null = await Categoria.findOneBy({ id_categoria:id });
 
-    let voluntario: Voluntario | null = await Voluntario.findOneBy({ idvoluntario: id });
-    if (!voluntario) {
-      return res.status(422).json({ error: "Usuário não foi encontrado!" });
-    }
-
-      (voluntario.nome = body.nome),
-      (voluntario.email = body.email),
-      (voluntario.senha = body.senha),
-      await voluntario.save();
+    // if (!categoria) {
+    //   return res.status(422).json({ error: "Categoria não encontrada!" });
+    // }
+    voluntario.nome = body.nome;
+    voluntario.email = body.email;
+    voluntario.senha = body.senha;
+    voluntario.situacao = "A";
+    voluntario.senha =  await bcrypt.hash(voluntario.senha, 10);;
+    await voluntario.save();
 
     return res.status(200).json(voluntario);
   }
 
   async delete(req: Request, res: Response): Promise<Response> {
+    let body = req.body;
+    let voluntario: Voluntario = res.locals.voluntario;
+    voluntario.situacao = "I";
+    await voluntario.save();
+    return res.status(200).json();
+  }
+
+  async find(req: Request, res: Response): Promise<Response> {
+    let body = req.body;
     let id = Number(req.params.id);
 
-    let voluntario: Voluntario | null = await Voluntario.findOneBy({ idvoluntario: id });
+    let voluntario: Voluntario | null = await Voluntario.findOneBy({
+      idvoluntario: id,
+    });
+
     if (!voluntario) {
-      return res.status(422).json({ error: "Usuário não foi encontrado!" });
+      return res.status(422).json({ error: "Voluntário não encontrado" });
     }
 
-    voluntario.remove;
-
-    return res.status(200).json({ text: `Usuário deletado!` });
+    return res.status(200).json(voluntario);
   }
 
   async checker(req: Request, res: Response): Promise<Response> {
     let nome;
-    let voluntario: Voluntario | null = await Voluntario.findOneBy({ nome: nome });
+    let voluntario: Voluntario | null = await Voluntario.findOneBy({
+      nome: nome,
+    });
 
     if (!voluntario) {
       return res.status(422).json({ error: "Usuário não foi encontrado!" });
@@ -78,8 +102,10 @@ export class VoluntarioController {
     let body = req.body;
     let id = Number(req.params.id);
 
-    let voluntario: Voluntario | null = await Voluntario.findOneBy({ idvoluntario: id });
-    if (voluntario?.situacao != 'A' ) {
+    let voluntario: Voluntario | null = await Voluntario.findOneBy({
+      idvoluntario: id,
+    });
+    if (voluntario?.situacao != "A") {
       res.status(422).json({ error: "Usuário não esta logado!" });
     } else {
       res.status(200).json({ text: `Usuário encontrado!` });
@@ -93,6 +119,7 @@ export class VoluntarioController {
   async setPassword(password: string): Promise<string> {
     const saltRounds = 10; // Número de rounds de hashing (quanto maior, mais seguro)
     const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log(hashedPassword);
     return hashedPassword;
   }
 
--- src/routes/voluntarios.ts
+++ src/routes/voluntarios.ts
@@ -1,17 +1,63 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { VoluntarioController } from "../controllers/VoluntariosController";
+import * as yup from "yup";
+import { Voluntario } from "../models/Voluntarios";
 let voluntarioController: VoluntarioController = new VoluntarioController();
+
+async function validarPayload(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    let schema = yup.object({
+      nome: yup.string().min(3).max(255).required(),
+      email: yup.string().email().min(3).max(255).required(),
+      senha:yup.string().min(3).max(255).required(),
+      situacao:yup.string().min(1).max(1).required()
+    });
+    let payload = req.body;
+    try {
+      req.body = await schema.validate(payload, {
+        abortEarly: false,
+        stripUnknown: true,
+      });
+      return next();
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        return res.status(400).json({ erros: error.errors });
+      }
+      return res.status(500).json({ error: "ops" });
+    }
+  }
+
+  async function validar(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    let id = Number(req.params.id);
+  
+    let voluntario: Voluntario | null = await Voluntario.findOneBy({ idvoluntario:id });
+  
+    if (!voluntario) {
+      return res.status(422).json({ error: "Voluntario não encontrada!" });
+    }
+    res.locals.voluntario = voluntario;
+  
+    return next();
+  }
+
 
 let rotas:Router = Router();
 //listar
 rotas.get("/voluntarios", voluntarioController.list);
 //visualizar 1 usuario pelo id
-// rotas.get("/voluntarios/:id",voluntarioController.find);
+rotas.get("/voluntarios/:id",validar,voluntarioController.find);
 //criar
-rotas.post("/voluntarios",voluntarioController.create);
+rotas.post("/voluntarios",validarPayload,voluntarioController.create);
 //atualizar
-rotas.put("/voluntarios/:id",voluntarioController.edit);
+rotas.put("/voluntarios/:id",validar,validarPayload,voluntarioController.edit);
 //delete
-rotas.delete("/voluntarios/:id",voluntarioController.delete);
+rotas.delete("/voluntarios/:id",validar,voluntarioController.delete);
 
 export default rotas;
