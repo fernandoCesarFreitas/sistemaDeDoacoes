ef6c4c4d43562bdddfcfb3c2637c25a77c5eb86f
Fernado Cesar Haupenthal de Freitas <139494640+fernandoCesarFreitas@users.noreply.github.com>
2023-10-05 18:02:03 -0300

    movimentacoes

--- src/routes/movimentacoes.ts
+++ src/routes/movimentacoes.ts
@@ -1,17 +1,67 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { MovimentacaoContrller } from "../controllers/MovimentacaoController";
+import * as yup from "yup";
+import { Movimentacao } from "../models/Movimentacao";
 let movimentacaoController: MovimentacaoContrller = new MovimentacaoContrller();
+
+async function validarPayload(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    let schema = yup.object({
+      tipo: yup.string().min(3).max(255).required(),
+      quantidade:yup.number().integer().required(),
+      doador: yup.string().min(3).max(255),
+      pessoa_id_pessoa: yup.number().integer(),
+      item_id: yup.number().integer(),
+      cd_id: yup.number().integer(),
+      cd_item_idcd_item: yup.number().integer(),
+    });
+    let payload = req.body;
+    console.log(payload)
+    try {
+      req.body = await schema.validate(payload, {
+        abortEarly: false,
+        stripUnknown: true,
+      });
+      return next();
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        return res.status(400).json({ erros: error.errors });
+      }
+      return res.status(500).json({ error: "ops" });
+    }
+  }
+
+  async function validar(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    let id = Number(req.params.id);
+  
+    let movimentacao: Movimentacao | null = await Movimentacao.findOneBy({ id_movimentacao:id });
+  
+    if (!movimentacao) {
+      return res.status(422).json({ error: "Movimentação não encontrada!" });
+    }
+    res.locals.movimentacao = movimentacao;
+  
+    return next();
+  }
+
 
 let rotas:Router = Router();
 //listar
 rotas.get("/movimentacao", movimentacaoController.list);
 //visualizar 1 usuario pelo id
-rotas.get("/movimentacao/:id",movimentacaoController.find);
+rotas.get("/movimentacao/:id",validar,movimentacaoController.find);
 //criar
-// rotas.post("/movimentacao",movimentacaoController.create);
+rotas.post("/movimentacao",validarPayload,movimentacaoController.create);
 //atualizar
-// rotas.put("/movimentacao/:id",movimentacaoController.edit);
+// rotas.put("/movimentacao/:id",validar,validarPayload,movimentacaoController.edit);
 //delete
-rotas.delete("/movimentacao/:id",movimentacaoController.delete);
+rotas.delete("/movimentacao/:id",validar,movimentacaoController.delete);
 
 export default rotas;
